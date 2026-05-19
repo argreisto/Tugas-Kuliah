27b06b00c0461a064066c51786c7fbf714b27963
Argreisto <argreistod@gmail.com>
2026-05-19 18:15:55 +0700

    Hello World Java pertama di git

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("\nHELLO WORLD");
+        System.out.println("Nama saya Argreisto Daniel, mahasiswa Teknik Informatika di Universitas Pamulang");
+        System.out.println("Program berhasil dijalankan");
+    }
+}
